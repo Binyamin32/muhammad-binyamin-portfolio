a0f91422d6cc19456360ab93ca131ff816b14bed
Muhammad Binyamin <biny3322@gmail.com>
2025-10-17 14:36:47 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/muhammad-binyamin-portfolio/', // ✅ important for GitHub Pages
+  server: { port: 5173 }
 })
